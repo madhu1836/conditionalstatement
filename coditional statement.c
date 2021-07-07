c78e1439a64d20032e70805c99abf0af41f48316
madhu1836 <86904017+madhu1836@users.noreply.github.com>
2021-07-07 21:54:15 +0530

    Create conditional statement.c

---
+++ coditional statement.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+int main()
+{ 
+  int n; 
+  printf("Enter marks:"); 
+  scanf("%d", &n); if (n>=85) 
+  { 
+    printf("Grade A"); 
+  } 
+  else if (n>=70 && n<=84) 
+  { 
+    printf("Grade B"); 
+  } 
+  else if (n>=55 && n<=69) 
+  { 
+    printf("Grade C"); 
+  } 
+  else if (n>40 && n<=54) 
+  { 
+    printf("Grade D"); 
+  } 
+  else 
+  { 
+    printf("Grade F"); 
+  } 
+}
